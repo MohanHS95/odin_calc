03115c350e7baa455b031c9c325758f403c0f729
MohanHS95 <mohanhsharan@gmail.com>
2025-01-08 12:13:42 -0500

    Add backspace,clear, and decimal functions
    These buttons each manipulate their associated arrays. +/- and percent
    buttons were removed for now as they are out of scope for the project.
    The interface was adjusted accordingly.
    
    Also functionality for rounding numbers in the display was added.

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ const resultBar = document.querySelector("#result-bar");
 
 const result = document.createElement("text");
 result.setAttribute("id", "result");
-result.textContent = undefined;
+result.textContent = 0;
 resultBar.appendChild(result);
 
 const buttons = document.querySelectorAll("button");
-const arr = [];
+let arr = [];
 
 // buttons.forEach((button) => {
 
@@ -83,7 +83,7 @@ buttons.forEach((button) => {
         // Handle the divide button
         if (button.classList.contains("operator") && !arr[1]) {
             if (nums.length > 0) {
-                let operand = parseInt(nums.join(''), 10);
+                let operand = parseFloat(result.textContent);
                 if (!isNaN(operand)) {
                     arr.push(operand);
                     arr.push(button.textContent);
@@ -94,14 +94,13 @@ buttons.forEach((button) => {
 
         }
 
-
-        if (button.classList.contains("equals")) {
+        if (button.classList.contains("equals") && arr[0]) {
             if (nums.length > 0) {
-                let operand = parseFloat(nums.join(''));
+                let operand = parseFloat(result.textContent);
                 if (!isNaN(operand)) {
                     arr.push(operand);
                     console.log(arr);
-                    result.textContent = operate(arr[0],arr[1],arr[2]);
+                    result.textContent = Math.round(operate(arr[0],arr[1],arr[2]) * 1000000000)/1000000000;
                     arr.splice(0,3, parseFloat(result.textContent));
                 }
                 nums = []; // Clear nums after processing
@@ -110,19 +109,78 @@ buttons.forEach((button) => {
 
         if (button.classList.contains("operator") && arr[1]) {
             if (nums.length > 0) {
-                let operand = parseFloat(nums.join(''));
+                let operand = parseFloat(result.textContent);
                 if (!isNaN(operand)) {
                     arr.push(operand);
                     console.log(arr);
-                    result.textContent = operate(arr[0],arr[1],arr[2]);
+                    result.textContent = Math.round(operate(arr[0],arr[1],arr[2]) * 1000000000)/1000000000;
                     arr.splice(0,3, parseFloat(result.textContent));
                     arr.push(button.textContent);
-
                 }
                 nums = []; // Clear nums after processing
             }
         }
 
+        if (button.classList.contains("operator") && arr[0] && !arr[1]){
+            arr.push(button.textContent);
+        }        
+
+        if (button.classList.contains("num") && arr[0] && !arr[1]) {
+            arr = [];
+            // result.textContent = nums.join('');
+        }
+
+        if (button.classList.contains("clear")) {
+            arr = [];
+            nums = [];
+            result.textContent = 0;
+        }
+
+        if (result.textContent == Infinity) {
+            const holdUp = "chillout";
+            result.textContent = holdUp;    
+        }
+
+        if (button.classList.contains("point")) {
+            if (nums.includes(".")) {;}
+            else nums.push(".");
+
+            if (result.textContent.includes(".")) {;}
+            else result.textContent += ".";
+        }
+
+        if (button.classList.contains("backspace")) {
+            if (nums.length > 0) {
+                nums.pop();
+                if (!nums[0]) {
+                result.textContent = 0;
+                }
+                // else result.textContent = 0;
+            }
+        }
+
+        // if (button.classList.contains("plusminus")) {
+        //     if (arr.length > 0) {
+        //         arr[0] = arr[0] * (-1);
+        //         result.textContent = arr[0];
+                
+        //     }
+        //     else result.textContent = nums.join('') * (-1);
+        //     // console.log(nums);
+        //     // nums.splice(0,nums.length,result.textContent);
+        //     // console.log(nums);
+        //     // console.log(arr);
+        //             // nums.push(result.textContent);
+
+        // }
+
+        // if (button.classList.contains("percent")) {
+        //     if(arr.length > 0 && !arr[1]) {
+        //         arr[0] = arr[0]/100;
+        //         result.textContent = arr[0];
+        //     }
+        //     else result.textContent = result.textContent / 100
+        // }
 
     });
 });
